7f78443401241ae3bc1fbab157eb190edf7d8c58
Yanqing Wu <y433wu@edu.uwaterloo.ca>
2020-06-16 23:54:18 -0400

    extract financial news

---
+++ fin-news/extractor.py
@@ -0,0 +1,40 @@
+from requests import get
+from requests.exceptions import RequestException
+from contextlib import closing
+from bs4 import BeautifulSoup
+
+
+def simple_get(url):
+    """
+    Attempts to get the content at `url` by making an HTTP GET request.
+    If the content-type of response is some kind of HTML/XML, return the
+    text content, otherwise return None.
+    """
+    try:
+        with closing(get(url, stream=True)) as resp:
+            if is_good_response(resp):
+                return resp.content
+            else:
+                return None
+
+    except RequestException as e:
+        log_error('Error during requests to {0} : {1}'.format(url, str(e)))
+        return None
+
+
+def is_good_response(resp):
+    """
+    Returns True if the response seems to be HTML, False otherwise.
+    """
+    content_type = resp.headers['Content-Type'].lower()
+    return (resp.status_code == 200
+            and content_type is not None
+            and content_type.find('html') > -1)
+
+
+def log_error(e):
+    print(e)
+
+
+#print(simple_get('https://www.ft.com/content/15a1cc68-4ab8-4acf-b230-7fe331adca33'))
+print(simple_get('https://www.ft.com'))
